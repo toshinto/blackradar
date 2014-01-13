7a09011a78ec401e35bb9d7b90becb4011fe294a
tosihnto <lyvcheto@abv.bg>
2014-01-13 13:46:45 +0200

    sad

--- blackradar-toshko/Form1.Designer.cs
+++ blackradar-toshko/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(26, 24);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(254, 125);
+            this.button1.Size = new System.Drawing.Size(165, 131);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
